3f527defc9918483df3cfb00e147ca66adbb07f3
Morgon Branning <MRBranning@student.fullsail.edu>
2025-12-16 23:33:34 -0500

    Created Sign Up screen, moved Log In screen out of index; rerouted navigation loop from dashboard, log in, sign up

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,6 +6,9 @@ import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -17,10 +20,17 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+          tabBarIcon: ({ color }) => <Entypo size={28} name="home" color={color} />
+        }}
+      />
+      <Tabs.Screen
         name="camera"
         options={{
           title: 'Camera',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="camera" color={color} />,
         }}
       />
     </Tabs>

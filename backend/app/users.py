dde3fc9f911bfade4caf229b00dfd5ced270d43d
Morgon Branning <MRBranning@student.fullsail.edu>
2025-12-18 02:44:58 -0500

    Created JWT authentication backend

--- backend/app/users.py
+++ backend/app/users.py
@@ -1,11 +1,19 @@
 import uuid
 
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin, models
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    BearerTransport,
+    JWTStrategy
+)
+from fastapi_users.db import SQLAlchemyUserDatabase
 
 from .db import User, get_user_db
 
 SECRET = "SECRET"
+
+
 
 
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
@@ -30,3 +38,22 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
+
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+def get_jwt_strategy() -> JWTStrategy[models.UP, models.ID]:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy
+)
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    [auth_backend]
+)
+
+current_active_user = fastapi_users.current_user(active=True)

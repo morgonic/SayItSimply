ab85791a777fb981e214c1a5ddf63a2c3b2db0aa
BuildTools <unconfigured@null.spigotmc.org>
2025-12-20 20:55:34 -0500

    added red (color matched to logo) bookmark icon to selected nav bar slot

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,8 +5,49 @@ import { HapticTab } from '@/components/haptic-tab';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
+import { View, StyleSheet } from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+type Props = {
+  focused: boolean;
+  color: string;
+  icon:
+    | { lib: "FontAwesome"; name: React.ComponentProps<typeof FontAwesome>["name"] }
+    | { lib: "Entypo"; name: React.ComponentProps<typeof Entypo>["name"] };
+};
+
+export function TabIcon({ focused, color, icon}: Props) {
+  return (
+    <View style={styles.container}>
+      {icon.lib === "FontAwesome" ? (
+        <FontAwesome name={icon.name} size={26} color={color} />
+      ) : (
+        <Entypo name={icon.name} size={26} color={color} />
+      )}
+
+      {focused && (
+        <View style={styles.bookmark}>
+          <FontAwesome name="bookmark" size={10} color={"#8c311cff"} />
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+    width: 32,
+    height: 32,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bookmark: {
+    position: "absolute",
+    right: -2,
+    top: -2,
+  },
+});
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -22,35 +63,65 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Dashboard",
-          tabBarIcon: ({ color }) => <Entypo size={28} name="home" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              icon={{ lib: "Entypo", name: "home" }}
+              />
+          ),
         }}
       />
       <Tabs.Screen
         name="documents"
         options={{
-          title: 'Documents',
-          tabBarIcon: ({ color }) => <Entypo size={28} name="documents" color={color} />,
+          title: "Documents",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              icon={{ lib: "Entypo", name: "documents" }}
+              />
+          ),
         }}
       />
       <Tabs.Screen
         name="camera"
         options={{
-          title: 'Camera',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="camera" color={color} />,
+          title: "Camera",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              icon={{ lib: "FontAwesome", name: "camera" }}
+              />
+          ),
         }}
       />
       <Tabs.Screen
         name="todo-list"
         options={{
-          title: 'To-Do List',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="list" color={color} />,
+          title: "To-Do List",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              icon={{ lib: "FontAwesome", name: "list" }}
+              />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+          title: "Profile",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              focused={focused}
+              color={color}
+              icon={{ lib: "Entypo", name: "user" }}
+              />
+          ),
         }}
       />
     </Tabs>

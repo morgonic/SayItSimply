b0262b4b90a1f6b5a52100cf25845c95fde21fae
Morgon Branning <MRBranning@student.fullsail.edu>
2026-01-08 17:29:49 -0500

    Create user variables to track onboarding completion status

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -5,11 +5,14 @@ from fastapi_users import schemas
 class UserRead(schemas.BaseUser[uuid.UUID]):
     language: str
     reading_level: int
+    onboarding_done: bool
 
 class UserCreate(schemas.BaseUserCreate):
     language: str = "en"
     reading_level: int = 6
+    onboarding_done: bool = False
 
 class UserUpdate(schemas.BaseUserUpdate):
     language: Optional[str] = None
     reading_level: Optional[int] = None
+    onboarding_done: Optional[bool] = None
